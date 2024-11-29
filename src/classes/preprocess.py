25e099cd5a13a2618fdf92646fca0401302e2120
migo <migo.vandingo@gmail.com>
2024-11-29 14:55:07 -0700

    stage2 preprocess data

--- src/classes/preprocess.py
+++ src/classes/preprocess.py
@@ -10,11 +10,12 @@ system_logger = setup_logger(os.environ['SYSTEM_LOG_FILE'], 'main_logger')
 class Preprocess:
     
     @staticmethod
-    def preprocess_ground_truth(video_directory: str, trim_list_file: str, output_shape: tuple, output_file: str):
+    def preprocess_ground_truth(video_directory: str, trim_list_file: str, output_shape: tuple, output_file: str, data_aug_flag: bool):
         try:
             """
             Preprocess trims and save as a .pt file.
             """
+            system_logger.info(f"Preprocessing data from: {trim_list_file}")
             trims = []
 
             # Load trims from file
@@ -26,16 +27,16 @@ class Preprocess:
                     label = int(label.rstrip())
 
                     # Preprocess video
-                    video_tensor = load_to_tensor_using_cv2(oshape=output_shape, data_aug_flag=True)
+                    video_tensor = load_to_tensor_using_cv2(video_path, oshape=output_shape, data_aug_flag=data_aug_flag)
 
                     # Append processed data
                     trims.append((label, video_tensor))
 
             # Save as .pt file
             torch.save(trims, output_file)
-            print(f"Preprocessed data saved to {output_file}")
+            system_logger.info(f"Preprocessed data saved to: {output_file}")
         except FileNotFoundError:
-            print(f"{__class__.__name__} -- {traceback.format_exc()} -- Error file not found: {trim_list_file}")
+            system_logger.info(f"{__class__.__name__} -- {traceback.format_exc()} -- Error file not found: {trim_list_file}")
         except Exception as e:
             system_logger.error(f"{__class__.__name__} -- {traceback.format_exc()} - {e} Error: {e}")
 
--- src/stages/preprocess_data.py
+++ src/stages/preprocess_data.py
@@ -7,7 +7,7 @@ system_logger = setup_logger(os.environ['SYSTEM_LOG_FILE'], 'main_logger')
 if __name__ == '__main__':
     system_logger.info("STAGE: preprocess_data")
     config = LoadUtils.load_yaml(os.environ['PARAMS_PATH'])
-    data = Preprocess.preprocess_ground_truth(video_directory=config['video_directory'], 
+    data = Preprocess.preprocess_ground_truth(video_directory=config['preprocess']['video_dir'], 
                                        trim_list_file=config['preprocess']['trim_list_file'], 
                                        output_shape=config['preprocess']['output_shape'], 
                                        output_file=config['preprocess']['output_file_name'],
